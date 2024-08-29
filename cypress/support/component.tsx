7fc81c9dec01f9eb25fde751eedc346968908d74
David Johnson <davemoses3@gmail.com>
2024-08-29 14:32:04 +0100

    css

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -1,17 +1,4 @@
-// cypress/support/index.ts
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { mount } from "cypress/react18";
+import "../../src/index.css";
 
-Cypress.Commands.add("mount", (component, options = {}) => {
-  // Use the default store if one is not provided
-  const queryClient = new QueryClient();
-
-  const { ...mountOptions } = options;
-
-  const wrapped = (
-    <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>
-  );
-
-  return mount(wrapped, mountOptions);
-});
+Cypress.Commands.add("mount", mount);
